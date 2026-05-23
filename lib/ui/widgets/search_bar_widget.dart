da0058b274718382c0f12029da7b8f76145cb920
mrvaloz <mrvaloz@gmail.com>
2026-05-22 22:48:43 -0500

    fix: replace AnimationController with AnimatedSize to avoid ConsumerStatefulWidget type error
    SingleTickerProviderStateMixin conflicted with ConsumerStatefulWidget type checking. Replaced with AnimatedSize which animates without needing a TickerProvider.

--- lib/ui/widgets/search_bar_widget.dart
+++ lib/ui/widgets/search_bar_widget.dart
@@ -9,51 +9,34 @@ class TaulSearchBar extends ConsumerStatefulWidget {
   ConsumerState<TaulSearchBar> createState() => _TaulSearchBarState();
 }
 
-class _TaulSearchBarState extends ConsumerState<TaulSearchBar>
-    with SingleTickerProviderStateMixin {
+class _TaulSearchBarState extends ConsumerState<TaulSearchBar> {
   final _controller = TextEditingController();
   bool _isOpen = false;
-  late final AnimationController _animCtrl;
-  late final Animation<double> _expandAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _animCtrl = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 250),
-    );
-    _expandAnimation = CurvedAnimation(
-      parent: _animCtrl,
-      curve: Curves.easeInOut,
-    );
-  }
 
   @override
   void dispose() {
     _controller.dispose();
-    _animCtrl.dispose();
     super.dispose();
   }
 
-  void _open() {
-    setState(() => _isOpen = true);
-    _animCtrl.forward();
-  }
+  void _open() => setState(() => _isOpen = true);
 
   void _close() {
     _controller.clear();
     ref.read(entrySearchProvider.notifier).state = '';
-    _animCtrl.reverse().then((_) {
-      if (mounted) setState(() => _isOpen = false);
-    });
+    setState(() => _isOpen = false);
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-      child: _isOpen ? _buildSearchInput() : _buildSearchIcon(),
+      child: AnimatedSize(
+        duration: const Duration(milliseconds: 250),
+        curve: Curves.easeInOut,
+        alignment: Alignment.centerLeft,
+        child: _isOpen ? _buildSearchInput() : _buildSearchIcon(),
+      ),
     );
   }
 
@@ -70,30 +53,26 @@ class _TaulSearchBarState extends ConsumerState<TaulSearchBar>
   }
 
   Widget _buildSearchInput() {
-    return SizeTransition(
-      sizeFactor: _expandAnimation,
-      axisAlignment: -1,
-      child: TextField(
-        controller: _controller,
-        autofocus: true,
-        onChanged: (value) =>
-            ref.read(entrySearchProvider.notifier).state = value,
-        decoration: InputDecoration(
-          hintText: 'Buscar en Taúl...',
-          prefixIcon: const Icon(Icons.search, size: 20),
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.close, size: 20),
-            onPressed: _close,
-            tooltip: 'Cerrar',
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-          ),
-          filled: true,
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-          isDense: true,
+    return TextField(
+      controller: _controller,
+      autofocus: true,
+      onChanged: (value) =>
+          ref.read(entrySearchProvider.notifier).state = value,
+      decoration: InputDecoration(
+        hintText: 'Buscar en Taúl...',
+        prefixIcon: const Icon(Icons.search, size: 20),
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.close, size: 20),
+          onPressed: _close,
+          tooltip: 'Cerrar',
         ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        filled: true,
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        isDense: true,
       ),
     );
   }
