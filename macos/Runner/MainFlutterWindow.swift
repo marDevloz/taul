b696e58dc615a4f38ec556907081a2c6dd51ca7e
mrvaloz <mrvaloz@gmail.com>
2026-05-26 00:55:59 -0500

    feat: set default window size to 400x700 for Linux and macOS

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -4,9 +4,11 @@ import FlutterMacOS
 class MainFlutterWindow: NSWindow {
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
+    let windowFrame = NSRect(x: 0, y: 0, width: 400, height: 700)
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
+    self.center()
+    self.minSize = NSSize(width: 300, height: 400)
 
     RegisterGeneratedPlugins(registry: flutterViewController)
 
