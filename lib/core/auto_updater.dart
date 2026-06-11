842d11ad39835d870af071a046b8f3deb3c26d07
mrvaloz <mrvaloz@gmail.com>
2026-06-11 07:12:31 -0500

    chore: bump version to 1.2.1

--- lib/core/auto_updater.dart
+++ lib/core/auto_updater.dart
@@ -9,7 +9,7 @@ const _manifestUrl =
     'https://github.com/marDevloz/taul/releases/latest/download/manifest.json';
 
 /// Versión actual del app (sincronizada con pubspec.yaml).
-const String appVersion = '1.2.0';
+const String appVersion = '1.2.1';
 
 /// Clave de SharedPreferences para la versión saltada.
 const _skipPrefKey = 'update_skip_version';
